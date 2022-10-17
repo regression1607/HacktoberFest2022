1c9bf06538044b2fa8ef42d261eb4062f7186c4c
Biswa Baibhab Subudhi <94667039+biswabaibhab007@users.noreply.github.com>
2022-10-17 20:00:42 +0530

    created Greeting.java

---
+++ Greeting.java
@@ -0,0 +1,8 @@
+public class Greeting {
+    public static void main(String[] args) {
+        greeting();
+    }
+    static void greeting() {
+        System.out.println("Hello There! ");
+    }
+}
