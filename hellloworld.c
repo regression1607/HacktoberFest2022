f31df5517b8ebcc638ff07b442a9e1091c9b173f
anushka bhandari <bhandarianushka670@gmail.com>
2022-10-25 19:26:55 +0530

    added program in c

---
+++ hellloworld.c
@@ -0,0 +1,12 @@
+// Header file for input output functions
+#include <stdio.h>
+  
+// main function -
+int main()
+{
+  
+    // prints hello world
+    printf("Hello World");
+  
+    return 0;
+}
